4289b85f6727ea5645424853bc74a7ed7e7866fa
santosh78-sys <61026956+santosh78-sys@users.noreply.github.com>
2022-01-05 11:02:18 -0800

    Add files via upload

---
+++ login.js
@@ -0,0 +1,13 @@
+const loginForm = document.getElementById('login-form');
+loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const loginEmail = loginForm['login-email'].value;
+    const loginPassword = loginForm['login-password'].value;
+    auth.signInWithEmailAndPassword(loginEmail, loginPassword).then(() => {
+        console.log('login success');
+        location = "users.html";
+    }).catch(err => {
+        const loginError = document.getElementById("loginError");
+        loginError.innerText = err.message;
+    })
+})
